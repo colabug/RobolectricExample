bda1035224031c9bf826f5b7ef58a8802e089302
Corey Leigh Latislaw <colabug@gmail.com>
2012-07-23 13:38:24 -0400

    Added behavior to button on new activity
    * Added click listener to button in new activity
    * Tested that the function doSomethingCrazy() was invoked
    * Added dummy function to override in the tests to ensure a condition was met
    * Created a test class that inherits from the class under test
    * Promoted test variable to field
    * Changed button and string names to match new functionality

--- src/main/java/com/chariotsolutions/colabug/NewActivity.java
+++ src/main/java/com/chariotsolutions/colabug/NewActivity.java
@@ -4,12 +4,24 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         setContentView(R.layout.new_activity);
+        setContentView(R.layout.new_activity);
+
+        View button = findViewById(R.id.crazy_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                doSomethingCrazy();
+            }
+        });
+    }
+
+    protected void doSomethingCrazy()
+    {
     }
 
     public static Intent createIntent(Context context) {
--- src/test/java/com/chariotsolutions/colabug/NewActivityTest.java
+++ src/test/java/com/chariotsolutions/colabug/NewActivityTest.java
@@ -1,5 +1,7 @@
 package com.chariotsolutions.colabug;
 
+import android.view.View;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,18 +9,22 @@ import org.junit.runner.RunWith;
 import support.ProjectTestRunner;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static support.ProjectTestRunner.assertViewIsVisible;
 
 @RunWith(ProjectTestRunner.class)
 
 public class NewActivityTest {
 
-    private NewActivity activity;
+    private TestNewActivity activity;
+    private View button;
 
     @Before
     public void setUp() throws Exception {
-        activity = new NewActivity();
+        activity = new TestNewActivity();
         activity.onCreate(null);
+
+        button = activity.findViewById(R.id.crazy_button);
     }
 
     @Test
@@ -28,6 +34,21 @@ public class NewActivityTest {
 
     @Test
     public void shouldHaveButton() throws Exception {
-        assertViewIsVisible(activity.findViewById(R.id.back_screen_button));
+        assertViewIsVisible(button);
+    }
+
+    @Test
+    public void buttonShouldDoSomethingCrazy() throws Exception {
+        button.performClick();
+        assertTrue(activity.conditionWasMet);
+    }
+
+    class TestNewActivity extends NewActivity {
+        protected boolean conditionWasMet = false;
+
+        @Override
+        protected void doSomethingCrazy() {
+            conditionWasMet = true;
+        }
     }
 }
