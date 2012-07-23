3194ffec23aaa49657efc66587453f9def5574b8
Corey Leigh Latislaw <colabug@gmail.com>
2012-07-23 11:00:45 -0400

    Added test for presence of textview
    * Added id to XML
    * Added setup method, made activity a field, and called on create
    * Added test for the visibility of the textview

--- src/test/java/com/chariotsolutions/colabug/MyActivityTest.java
+++ src/test/java/com/chariotsolutions/colabug/MyActivityTest.java
@@ -1,17 +1,37 @@
 package com.chariotsolutions.colabug;
 
+import android.view.View;
+
 import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 @RunWith(RobolectricTestRunner.class)
 
 public class MyActivityTest {
+
+    private MyActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = new MyActivity();
+        activity.onCreate(null);
+    }
+
     @Test
     public void shouldNotBeNull() throws Exception {
-        MyActivity activity = new MyActivity();
         assertNotNull(activity);
+    }
+
+    @Test
+    public void shouldHaveWelcomeText() throws Exception {
+        assertThat(activity.findViewById(R.id.welcome_text_view).getVisibility(),
+                   equalTo(View.VISIBLE));
     }
 }
